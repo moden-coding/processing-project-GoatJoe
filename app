29834b7195a653429a17a2dbe7cfa1b3d73443ff
GoatJoe <29nzadeh@gannacademy.org>
2025-10-22 09:23:12 -0400

    hi

---
+++ app
@@ -0,0 +1,1 @@
+Import.prossesing.core.*;
--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ import processing.core.*;
 
 public class App extends PApplet{
     boolean colorred, colorblue, colorgreen;
-    int circlex = -400; 
-    int circley = -400;
+    int circlex = 400; 
+    int circley = 400;
     public static void main(String[] args)  {
         PApplet.main("App");
     }
@@ -15,11 +15,11 @@ public class App extends PApplet{
     }
 
     public void settings(){
-        
+        size(800,800);
     }
 
     public void draw(){
-        background(105);
+        background(10,0,255);
        ellipse (circlex, circley, 50, 70);
        rect(200, 0, 250, 0);
      if (colorred == true){;
@@ -31,8 +31,13 @@ public class App extends PApplet{
     if (colorblue == true){
         circlex += 400;
     }
+    if (frameRate%100 == 0){
+        fill (random(255));
+    }
     }
     public void keypressed(){
-     
+     if (keyCode == LEFT) {
+        circlex -= 10;
+     }
     }
 }
