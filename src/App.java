1df8f174bc7d84c72e90153c09f8b32e74073bd9
GoatJoe <29nzadeh@gannacademy.org>
2025-10-24 13:02:02 -0400

    hi

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
 import processing.core.*;
+import processing.core.PShapeSVG.LineOfText;
 
 public class App extends PApplet{
     boolean colorred, colorblue, colorgreen;
-    int circlex = 400; 
-    int circley = 400;
+    int circlex = 700; 
+    int circley = 530;
+    int rectr = 0;
+    PImage football;
     public static void main(String[] args)  {
         PApplet.main("App");
     }
@@ -12,18 +15,30 @@ public class App extends PApplet{
         colorblue = false;
         colorgreen = false;
         colorred = false;
+        football = loadImage("Screenshot 2025-10-24 125417.png");
     }
 
     public void settings(){
-        size(800,800);
+        size(800,600);
     }
 
     public void draw(){
         background(10,0,255);
+        fill (40,20,20);
        ellipse (circlex, circley, 50, 70);
-       rect(200, 0, 250, 0);
+       fill (200,0, 41);
+       rect(20, 50, 50, 120);
+       fill(255, 255, 0);
+       rect (300, 60, 20, 260);
+       fill(255, 255, 0);
+       rect (500, 60, 20, 260);
+       fill(255, 255, 0);
+       rect (300, 320, 220, 30);
+       rect (400, 350, 20, 200);
+       imageMode(CENTER);
+       image(football, 200,200,45,50);
      if (colorred == true){;
-        circlex -= 400;
+        circlex -= 40;
     }
     if (colorgreen == true){
         circlex += 0;
@@ -37,7 +52,8 @@ public class App extends PApplet{
     }
     public void keypressed(){
      if (keyCode == LEFT) {
-        circlex -= 10;
+    circlex -= 100;    
+    circley -= 100;
      }
     }
 }
